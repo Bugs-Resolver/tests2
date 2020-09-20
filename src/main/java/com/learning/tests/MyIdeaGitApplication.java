67d62c19062c0b5052cfadd569d6b3fa889c77e0
alex <bugs.resolver@gmail.com>
2020-09-20 13:42:17 -0600

    update1

---
+++ src/main/java/com/learning/tests/MyIdeaGitApplication.java
@@ -0,0 +1,13 @@
+package com.learning.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyIdeaGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyIdeaGitApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/learning/tests/controller/MyController.java
@@ -0,0 +1,17 @@
+package com.learning.tests.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @GetMapping("/test1")
+    public String test1(){
+        System.out.println("working");
+        System.out.println("update1");
+        System.out.println("update2");
+        System.out.println("hello");
+        return "success";
+    }
+}
